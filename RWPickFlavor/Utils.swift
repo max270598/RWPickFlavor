f969f35b672cdb619aaac18798285faa9a648efa
MaxOl <some@mail.com>
2022-11-03 16:39:25 +0300

    Initial commit

---
+++ RWPickFlavor/Utils.swift
@@ -0,0 +1,19 @@
+//
+//  Utils.swift
+//  RWPickFlavor
+//
+//  Created by Мах Ol on 03.11.2022.
+//
+
+import Foundation
+import Alamofire
+
+public class Utils {
+    public func makeSum(a: Int, b: Int) -> Int {
+        a + b
+    }
+    
+    public func makeAF() {
+        let session = SessionManager().session
+    }
+}
